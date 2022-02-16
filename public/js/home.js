043c4f606246a394668f77121b17923e072d050e
Victoria Cardoso Soares <victoriacardoso0905@gmail.com>
2022-02-16 16:24:04 -0300

    alteração

--- public/js/home.js
+++ public/js/home.js
@@ -29,6 +29,8 @@ document.getElementById("transaction-form").addEventListener("submit", function(
     getCashIn();
     getCashOut();
     getTotal();
+
+    getCashIn();
     
     alert("Lançamento adicionado com sucesso.");
 });
@@ -81,14 +83,22 @@ function getCashIn() {
         }
  
         for (let index = 0; index < limit; index++) {
-           cashInHtml += ``
-            
-           
-
-           
-           
-            
-            
+           cashInHtml += `
+           <div class="row mb-4">
+               <div class="col-12">
+                   <h3 class="fs-2"> R$ ${cashIn[index].value.toFixed(2)}</h3>
+                   <div class="container p-0">
+                       <div class="row">
+                           <div class="col-12 col-md-8">
+                               <p>${cashIn[index].description}</p>
+                       </div>
+                       <div class="col-12 col-md-3 d-flex justify-content-end">
+                           ${cashIn[index].date}
+                       </div>
+                   </div>
+               </div>
+           </div>
+            `
         }
 
         document.getElementById("cash-in-list").innerHTML = cashInHtml;
@@ -135,6 +145,7 @@ function getTotal() {
        } else {
            total -= item.value;
        }
+   
     });
 
     document.getElementById("total").innerHTML = `R$ ${total.toFixed(2)}`;
