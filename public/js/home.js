a7fffcb46045a90b9f752fd25cecabbe09bed20e
Victoria Cardoso Soares <victoriacardoso0905@gmail.com>
2022-02-17 19:43:44 -0300

    alteração

--- public/js/home.js
+++ public/js/home.js
@@ -23,14 +23,12 @@ document.getElementById("transaction-form").addEventListener("submit", function(
     });
 
     saveData(data);
-    e.target.resert();
+    e.target.reset();
     myModal.hide();
 
     getCashIn();
     getCashOut();
     getTotal();
-
-    getCashIn();
     
     alert("Lançamento adicionado com sucesso.");
 });
@@ -52,8 +50,6 @@ function checkLogged() {
     if(dataUser) {
         data = JSON.parse(dataUser);
     }
-
-    getCashIn();
 
     getCashIn();
     getCashOut();
@@ -85,12 +81,12 @@ function getCashIn() {
         for (let index = 0; index < limit; index++) {
            cashInHtml += `
            <div class="row mb-4">
-               <div class="col-12">
-                   <h3 class="fs-2"> R$ ${cashIn[index].value.toFixed(2)}</h3>
-                   <div class="container p-0">
-                       <div class="row">
-                           <div class="col-12 col-md-8">
-                               <p>${cashIn[index].description}</p>
+           <div class="col -12">
+               <h3 class="fs-2">R$ ${cashIn[index].value.toFixed(2)}</h3>
+               <div class="container p-0">
+                   <div class="row">
+                       <div class="col-12 col-md-8">
+                           <p>${cashIn[index].description}</p>
                        </div>
                        <div class="col-12 col-md-3 d-flex justify-content-end">
                            ${cashIn[index].date}
@@ -98,11 +94,13 @@ function getCashIn() {
                    </div>
                </div>
            </div>
-            `
-        }
-
-        document.getElementById("cash-in-list").innerHTML = cashInHtml;
+       </div>
+        `
     }
+       
+       document.getElementById("cash-out-list").innerHTML = cashInHtml;
+    }
+
 }
 
 function getCashOut() {
